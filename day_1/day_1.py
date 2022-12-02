96bf1d0e13559017234687c0fbae3aca0cc788cc
Jaroslav Streit <101869862+xstrei06@users.noreply.github.com>
2022-12-02 01:20:38 +0100

    Create day_1.py
    1st day of AoC

---
+++ day_1/day_1.py
@@ -0,0 +1,23 @@
+f = open("data.txt", 'r')
+line = f.readline()
+max = 0
+elf = 0
+top2 = 0
+top3 = 0
+for line in f.readlines():
+    if line == '\n':
+        if max < elf:
+            top3 = top2
+            top2 = max
+            max = elf
+        elif top2 < elf:
+            top3 = top2
+            top2 = elf
+        elif top3 < elf:
+            top3 = elf
+        elf = 0
+    else:
+        elf += int(line)
+print('\n')
+print("most calories: " + str(max))
+print("top three: " + str(max + top2 + top3))
