df1ead4c1e13a1ba71ce1da156f053a6fadd42b2
Jaroslav Streit <Jarek.Streit@seznam.cz>
2022-12-05 19:55:30 +0100

    day_5 completed

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,63 +1,29 @@
 f = open("input.txt", "r")
-stack1 = "T D W Z V P"
-stack2 = "L S W V F J D"
-stack3 = "Z M L S V T B H"
-stack4 = "R S J"
-stack5 = "C Z B G F M L W"
-stack6 = "Q W V H Z R G B"
-stack7 = "V J P C B D N"
-stack8 = "P T B Q"
-stack9 = "H G Z R C"
-
-stack1 = stack1.split(" ")
-stack2 = stack2.split(" ")
-stack3 = stack3.split(" ")
-stack4 = stack4.split(" ")
-stack5 = stack5.split(" ")
-stack6 = stack6.split(" ")
-stack7 = stack7.split(" ")
-stack8 = stack8.split(" ")
-stack9 = stack9.split(" ")
-
+stacks = ["TDWZVP", "LSWVFJD", "ZMLSVTBH",
+          "RSJ", "CZBGFMLW", "QWVHZRGB",
+          "VJPCBDN", "PTBQ", "HGZRC"]
+stacks2 = ["TDWZVP", "LSWVFJD", "ZMLSVTBH",
+           "RSJ", "CZBGFMLW", "QWVHZRGB",
+           "VJPCBDN", "PTBQ", "HGZRC"]
 nline = 1
-stack_from = ""
 for line in f.readlines():
     if nline != 11:
         nline += 1
         continue
     line = line.replace("move ", "").replace("from ", "").replace("to ", "").strip("\n").split(" ")
-    print(line)
-    i = int(line[0])
-    match line[1]:
-        case "1":
-            stack_from = stack1
-        case "2":
-            stack_from = stack2
-        case "3":
-            stack_from = stack3
-        case "4":
-            stack_from = stack4
-        case "5":
-            stack_from = stack5
-        case "6":
-            stack_from = stack6
-        case "7":
-            stack_from = stack7
-        case "8":
-            stack_from = stack8
-        case "9":
-            stack_from = stack9
-        case _:
-            continue
-    print(stack_from)
-print(stack1)
-print(stack2)
-print(stack3)
-print(stack4)
-print(stack5)
-print(stack6)
-print(stack7)
-print(stack8)
-print(stack9)
-
+    for i in range(0, int(line[0])):
+        stacks[int(line[2]) - 1] = stacks[int(line[2]) - 1] + stacks[int(line[1]) - 1][-1]
+        stacks[int(line[1]) - 1] = stacks[int(line[1]) - 1][0:-1]
+    stacks2[int(line[2]) - 1] = stacks2[int(line[2]) - 1] + stacks2[int(line[1]) - 1][-(int(line[0])):]
+    stacks2[int(line[1]) - 1] = stacks2[int(line[1]) - 1][0:-(int(line[0]))]
+top = (stacks[0][-1] + stacks[1][-1]
+       + stacks[2][-1] + stacks[3][-1]
+       + stacks[4][-1] + stacks[5][-1]
+       + stacks[6][-1] + stacks[7][-1] + stacks[8][-1])
+top2 = (stacks2[0][-1] + stacks2[1][-1]
+        + stacks2[2][-1] + stacks2[3][-1]
+        + stacks2[4][-1] + stacks2[5][-1]
+        + stacks2[6][-1] + stacks2[7][-1] + stacks2[8][-1])
+print("part one: " + top)
+print("part two: " + top2)
 f.close()
